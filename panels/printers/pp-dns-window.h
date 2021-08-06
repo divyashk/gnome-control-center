4f36fe3321c1aaf04a2c448d0ae05f47f431b264
Divyasheel <dk0018w@gmail.com>
2021-08-06 10:19:00 +0530

    Made the DNS-browsing window a template widget. This will allow me to segregate the code from the already written cc-printer-panels file. All the requirements for this window will be fullfilled in a seperate file, thus making segregation easier.

---
+++ panels/printers/pp-dns-window.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <gtk/gtk.h>
+#include <cups/cups.h>
+G_BEGIN_DECLS
+
+#define PP_DNS_WINDOW_TYPE (pp_dns_window_get_type())
+G_DECLARE_FINAL_TYPE (PpDnsWindow, pp_dns_window, PP, DNS_WINDOW, GtkWindow)
+
+PpDnsWindow* pp_dns_window_new(cups_dest_t dummy);
+
+G_END_DECLS
